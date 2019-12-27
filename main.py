a2735eee80c1a2f828b07258153863d0a77cafb5
EveryoneHATEme <everyonehateme@yandex.ru>
2019-12-27 17:36:48 +0500

    Добавил ИИ. Ужасно тупой, двигается в основном по правилам рандома. Возникли трудности с реализацией волнового алгоритма. Буду думать. Позднее добавлю стрельбу для врагов.

--- main.py
+++ main.py
@@ -1,34 +1,50 @@
 import pygame
 import numpy as np
-
+from random import choice, randint
 
 WINDOW_SIZE = (1000, 700)
 PLAYGROUND_WIDTH = 700
 UP, DOWN, LEFT, RIGHT, SHOOT = pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT, pygame.K_SPACE
 BUTTONS = {UP, DOWN, LEFT, RIGHT, SHOOT}
 FPS = 30
+frames, seconds = 0, 0
+np.set_printoptions(linewidth=150)
 
 
 class Game:
     def __init__(self):
-        self.map = read_map('map1.txt').T  # загружаем карту из txt файла, возможно будем хранить по-другому
-        self.cell_size = PLAYGROUND_WIDTH // self.map.shape[1]
-        self.sprites = pygame.sprite.Group()
-        self.player = Player(0, 0, self.cell_size * 2 - 10, 30, self.sprites)
-        self.init_map()
         pygame.init()
+        self.players = pygame.sprite.Group()
+        self.sprites = pygame.sprite.Group()
+        self.map = self.read_map('map1.txt').T  # загружаем карту из txt файла, возможно будем хранить по-другому
+        self.cell_size = PLAYGROUND_WIDTH // self.map.shape[1]
+        self.init_map()
+        for player in self.players.sprites():
+            player.change_config(player.rect.x * self.cell_size, player.rect.y * self.cell_size,
+                                 self.cell_size * 2 - 10, 60)
         self.screen = pygame.display.set_mode(WINDOW_SIZE)
+        self.enemy = Enemy(0, 0, self.cell_size * 2 - 10, 30, self.map, self.sprites)
+        self.enemy = Enemy(PLAYGROUND_WIDTH // 2 - self.cell_size // 2, 0, self.cell_size * 2 - 10, 30, self.map, self.sprites)
         self.run = False
-        self.sprites.add(self.player)
+        self.clock = pygame.time.Clock()
 
     def main_loop(self):
+        global frames, seconds
         self.run = True
         while self.run:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.run = False
-                self.player.check_controls(event)
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_q:
+                    pass
+                    #self.enemy.update_path_map()
+                for player in self.players.sprites():
+                    player.check_controls(event)
             self.sprites.update()
+            self.clock.tick(FPS)
+            frames += 1
+            seconds += frames // FPS
+            frames %= FPS
             self.render()
 
     def render(self):
@@ -53,8 +69,21 @@ class Game:
             for j in range(PLAYGROUND_WIDTH // self.cell_size):
                 if self.map[i, j] == 1:
                     self.sprites.add(BrickWall(i * self.cell_size, j * self.cell_size, self.cell_size))
-                elif self.map[i, j] == 9:
-                    self.player.rect.topleft = (i * self.cell_size, j * self.cell_size)
+
+    def read_map(self, filename: str):
+        with open(filename) as file:
+            res = []
+            content = file.readlines()
+            for row in range(len(content)):
+                line = []
+                for col in range(len(content)):
+                    if content[row][col] == '9':
+                        Player(col, row, 0, 0, self.sprites, self.players)
+                        line.append(0)
+                    else:
+                        line.append(int(content[row][col]))
+                res.append(line)
+            return np.array(res, dtype=int, ndmin=1)
 
 
 class Tank(pygame.sprite.Sprite):
@@ -62,19 +91,26 @@ class Tank(pygame.sprite.Sprite):
         super().__init__(*groups)
         self.rect = pygame.Rect(x, y, cell_size, cell_size)
         self.velocity = velocity
-        self.vel_x, self.vel_y = 0, 0
-        self.move_dict = {key: False for key in BUTTONS}
+        self.vel_x, self.vel_y = 0, velocity
         self.image = pygame.Surface((cell_size, cell_size))
         self.facing = UP
         self.bullets = pygame.sprite.Group()
+        self.cell_size = cell_size
+
+    def change_config(self, x, y, cell_size, velocity):
+        self.rect = pygame.Rect(x, y, cell_size, cell_size)
+        self.velocity = velocity
+        self.image = pygame.Surface((cell_size, cell_size))
+        self.cell_size = cell_size
 
     def update(self, *args):
         self.rect.y += self.vel_y / FPS
         collided_count = len(pygame.sprite.spritecollide(self, self.groups()[0], False))
-        if collided_count > 1:
+        if collided_count > 1 or not (0 <= self.rect.y <= PLAYGROUND_WIDTH - self.rect.height):
             self.rect.y -= self.vel_y / FPS
         self.rect.x += self.vel_x / FPS
-        if len(pygame.sprite.spritecollide(self, self.groups()[0], False)) > 1:
+        collided_count = len(pygame.sprite.spritecollide(self, self.groups()[0], False))
+        if collided_count > 1 or not (0 <= self.rect.x <= PLAYGROUND_WIDTH - self.rect.width):
             self.rect.x -= self.vel_x / FPS
         if self.vel_x > 0:
             self.facing = RIGHT
@@ -87,9 +123,123 @@ class Tank(pygame.sprite.Sprite):
         self.bullets.update()
 
 
+class Enemy(Tank):
+    def __init__(self, x, y, cell_size, velocity, level_map, *groups):
+        super().__init__(x, y, cell_size, velocity, *groups)
+        self.image.fill((64, 64, 255))
+        self.map = level_map
+        self.path_map = self.map.copy()
+        self.path = []
+        self.mask = pygame.mask.from_surface(self.image)
+        #self.update_path_map()
+
+    def change_config(self, x, y, cell_size, velocity):
+        super().change_config(x, y, cell_size, velocity)
+        self.image.fill((64, 64, 255))
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def update(self, *args):
+        if self.rect.x % self.cell_size in range(2) and \
+                self.rect.y % self.cell_size in range(2) and randint(0, 7) == 0:
+            self.change_direction()
+        self.rect.y += self.vel_y / FPS
+        collided_count = len(pygame.sprite.spritecollide(self, self.groups()[0], False))
+        if collided_count > 1 or not (0 <= self.rect.y <= PLAYGROUND_WIDTH - self.rect.height):
+            self.rect.y -= self.vel_y / FPS
+            if (self.rect.x % self.cell_size not in range(2) or
+                    self.rect.y % self.cell_size not in range(2)) and randint(0, 3) == 0:
+                self.change_direction(invert=True)
+                print('invert')
+            else:
+                self.change_direction(rotate=choice([-1, 1]))
+                print('rotate')
+        self.rect.x += self.vel_x / FPS
+        collided_count = len(pygame.sprite.spritecollide(self, self.groups()[0], False))
+        if collided_count > 1 or not (0 <= self.rect.x <= PLAYGROUND_WIDTH - self.rect.width):
+            self.rect.x -= self.vel_x / FPS
+            if (self.rect.x % self.cell_size not in range(2) or
+                    self.rect.y % self.cell_size not in range(2)) and randint(0, 3) == 0:
+                self.change_direction(invert=True)
+                print('invert')
+            else:
+                self.change_direction(rotate=choice([-1, 1]))
+                print('rotate')
+
+    def change_direction(self, invert=False, rotate=0):
+        if invert:
+            self.vel_x = -self.vel_x
+            self.vel_y = -self.vel_y
+        elif rotate:
+            if self.vel_x == self.velocity:
+                self.vel_x, self.vel_y = 0, self.velocity * rotate
+            elif self.vel_x == -self.velocity:
+                self.vel_x, self.vel_y = 0, -self.velocity * rotate
+            elif self.vel_y == self.velocity:
+                self.vel_x, self.vel_y = -self.velocity * rotate, 0
+            elif self.vel_y == -self.velocity:
+                self.vel_x, self.vel_y = self.velocity * rotate, 0
+        else:
+            self.vel_x = self.velocity * randint(-1, 1)
+            if self.vel_x == 0:
+                self.vel_y = choice([-self.velocity, self.velocity])
+
+    def update_path_map(self):
+        self.path_map = np.zeros((self.map.shape[0] - 1, self.map.shape[1] - 1), dtype=int)
+
+        for i in range(self.map.shape[0] - 1):
+            for j in range(self.map.shape[1] - 1):
+                if [self.map[i, j], self.map[i, j + 1], self.map[i + 1, j], self.map[i + 1, j + 1]] == [0] * 4:
+                    self.path_map[i, j] = 0
+                else:
+                    self.path_map[i, j] = -1
+
+        player_x, player_y = 0, 0
+
+        for sprite in self.groups()[0]:
+            if isinstance(sprite, Enemy):
+                self.path_map[sprite.rect.left // self.path_map.shape[0],
+                              sprite.rect.top // self.path_map.shape[1]] = -1
+            if isinstance(sprite, Player):
+                player_x = sprite.rect.left // (self.cell_size + 10) * 2
+                player_y = sprite.rect.top // (self.cell_size + 10) * 2
+
+        self.initialize_map(self.rect.left // self.path_map.shape[0], self.rect.top // self.path_map.shape[1])
+
+        if self.path_map[player_x, player_y] != 0:
+            self.path = []
+            x, y = player_x, player_y
+            while self.path_map[x, y] != 1:
+                if x > 0 and self.path_map[x - 1, y] == self.path_map[x, y] - 1:
+                    x -= 1
+                elif x + 1 < self.path_map.shape[0] and self.path_map[x + 1, y] == self.path_map[x, y] - 1:
+                    x += 1
+                elif y > 0 and self.path_map[x, y - 1] == self.path_map[x, y] - 1:
+                    y -= 1
+                elif y + 1 < self.path_map.shape[1] and self.path_map[x, y + 1] == self.path_map[x, y] - 1:
+                    y += 1
+                self.path.append((x, y))
+            self.path = self.path[::-1]
+            del self.path[0]
+
+    def initialize_map(self, x, y, d=1):
+        self.path_map[x, y] = d
+        if x > 0 and (self.path_map[x - 1, y] == 0 or self.path_map[x - 1, y] > d):
+            self.initialize_map(x - 1, y, d + 1)
+        if x + 1 < self.path_map.shape[0] and (self.path_map[x + 1, y] == 0 or self.path_map[x + 1, y] > d):
+            self.initialize_map(x + 1, y, d + 1)
+        if y > 0 and (self.path_map[x, y - 1] == 0 or self.path_map[x, y - 1] > d):
+            self.initialize_map(x, y - 1, d + 1)
+        if y + 1 < self.path_map.shape[1] and (self.path_map[x, y + 1] == 0 or self.path_map[x, y + 1] > d):
+            self.initialize_map(x, y + 1, d + 1)
+
+
 class Player(Tank):
-    def __init__(self, x, y, cell_size, velocity, group):
-        super().__init__(x, y, cell_size, velocity, group)
+    def __init__(self, x, y, cell_size, velocity, *groups):
+        super().__init__(x, y, cell_size, velocity, *groups)
+        self.image.fill((64, 255, 64))
+
+    def change_config(self, x, y, cell_size, velocity):
+        super().change_config(x, y, cell_size, velocity)
         self.image.fill((64, 255, 64))
 
     def check_controls(self, event: pygame.event.EventType):
@@ -120,6 +270,7 @@ class Block(pygame.sprite.Sprite):
     TODO: надо проработать массив структуры стены. В оригинале был 4 * 4, думаю, лучше будет сделать так же.
         Также, надо придумать, как хранить структуру стен в txt файле.
     """
+
     def __init__(self, x, y, cell_size, *groups):
         super().__init__(*groups)
         self.rect = pygame.Rect(x, y, cell_size, cell_size)
@@ -158,10 +309,13 @@ class Bullet(pygame.sprite.Sprite):
     def update(self, *args):
         self.rect.centerx += self.velocity_x / FPS
         self.rect.centery += self.velocity_y / FPS
-        collide_with = get_collided_by_mask(self, self.owner.groups()[0])
+        collide_with = pygame.sprite.spritecollide(self, self.owner.groups()[0], False)
         if collide_with:
-            self.owner.groups()[0].remove(*collide_with)
-            self.terminate()
+            for sprite in collide_with:
+                if sprite is not self.owner and sprite is not self:
+                    self.owner.groups()[0].remove(sprite)
+                    self.terminate()
+                    break
         if self.rect.bottom < 0 or self.rect.top > PLAYGROUND_WIDTH:
             self.terminate()
         elif self.rect.right < 0 or self.rect.left > PLAYGROUND_WIDTH:
@@ -172,16 +326,10 @@ class Bullet(pygame.sprite.Sprite):
         del self
 
 
-def read_map(filename: str):
-    with open(filename) as file:
-        res = [[int(i) for i in line.strip()] for line in file.readlines()]
-        return np.array(res, dtype=int, ndmin=1)
-
-
 def get_collided_by_mask(sprite_1: pygame.sprite.Sprite, group: pygame.sprite.Group):
     collided = []
-    for sprite_2 in group.spritedict.keys():
-        if pygame.sprite.collide_mask(sprite_1, sprite_2) and sprite_1 is not sprite_2 and sprite_2:
+    for sprite_2 in group.sprites():
+        if pygame.sprite.collide_mask(sprite_1, sprite_2) and sprite_1 is not sprite_2:
             if isinstance(sprite_1, Bullet) and sprite_1.owner is sprite_2:
                 continue
             collided.append(sprite_2)
@@ -192,3 +340,35 @@ if __name__ == '__main__':
     game = Game()
     game.main_loop()
     pygame.quit()
+
+
+'''
+self.rect.y += self.vel_y / FPS
+        collided_count = len(pygame.sprite.spritecollide(self, self.groups()[0], False))
+        if collided_count > 1 or not (0 <= self.rect.y <= PLAYGROUND_WIDTH - self.rect.height):
+            self.rect.y -= self.vel_y / FPS
+        self.rect.x += self.vel_x / FPS
+        collided_count = len(pygame.sprite.spritecollide(self, self.groups()[0], False))
+        if collided_count > 1 or not (0 <= self.rect.x <= PLAYGROUND_WIDTH - self.rect.width):
+            self.rect.x -= self.vel_x / FPS
+'''
+"""
+        if frames == 0:
+            self.update_path_map()
+            left = self.rect.left // self.cell_size
+            right = self.rect.right // self.cell_size
+            top = self.rect.top // self.cell_size
+            bottom = self.rect.bottom // self.cell_size
+            index = 0
+            #if (left, top) == self.path[0] and right == self.path[0][0] + 2 and bottom == self.path[0][1] + 2:
+            #    index += 1
+            self.vel_x, self.vel_y = 0, 0
+            if self.path[index][0] < left:
+                self.vel_x = -self.velocity
+            elif self.path[index][0] > left:
+                self.vel_x = self.velocity
+            elif self.path[index][1] < top:
+                self.vel_y = -self.velocity
+            elif self.path[index][1] > top:
+                self.vel_y = self.velocity
+"""
